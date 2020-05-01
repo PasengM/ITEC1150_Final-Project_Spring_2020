f7cdd6a1e3419d01f679424c0a23849a1b745da9
Paseng Moua <vl4922gc@go.minneapolis.edu>
2020-05-01 18:32:25 -0500

    Project done TO DO comments

--- Final_Project.py
+++ Final_Project.py
@@ -8,26 +8,22 @@ Write the text "Random Taco Cookbook" on the image.
 Save the modified image to a new file. """
 
 # Imported libraries
-from PIL import Image, ImageDraw, ImageFont
-import requests
-import docx
+from PIL import Image, ImageDraw, ImageFont  # From the Pillow library import Image (add image), ImageDraw (draw on
+# image) and ImageFont (Write on it)
+import requests  # Request = get json data from API servers
+import docx  # Create my word document
 
-# CODE PROGRAM 1
-image = Image.open('Taco_spencer_davis_unsplash_Original.jpg')
+# CODE PROGRAM 1  (Working on the picture)
+image = Image.open('Taco_spencer_davis_unsplash_Original.jpg')  # I select the picture I'll work with
+sized_image = image.resize((800, 800))  # I resize the picture 800px * 800px
 
-width = image.width  # Width
-height = image.height  # Height
+image_draw = ImageDraw.Draw(sized_image)  # I'll draw on the resized picture, the 800px*800px so I create a new
+# variable for it
+font = ImageFont.truetype('DejaVuSans.ttf', 50)  # I'll use the typology DejaVuSans.ttf as font
+image_draw.text([100, 700], 'Random Taco Cookbook', fill='Fuchsia', font=font)  # I write 'Random Taco Cookbook'
+# starting 100px to the right and 700px down
 
-width_sized = int(width / 3)
-height_sized = int(height / 3.75)
-
-sized_image = image.resize((width_sized, height_sized))
-
-image_draw = ImageDraw.Draw(sized_image)
-font = ImageFont.truetype('DejaVuSans.ttf', 50)
-image_draw.text([100, 700], 'Random Taco Cookbook', fill='Fuchsia', font=font)
-
-sized_image.save('Image_Tacos_last_modified.jpg')
+sized_image.save('Image_Tacos_last_modified.jpg')  # I save this image with the name "Image_Tacos_last_modified.jpg"
 
 """Program 2: Make the Random Taco Recipe Book (40 points for code, 35 points for comments) Use requests to download 
 three random tacos from the random taco API. Save the data for each of three tacos in your program. Notice that each 
@@ -42,68 +38,54 @@ break" Repeat to write all of the next recipe and a page break. Repeat to write 
 document. """
 
 # CODE PROGRAM 2
+##################      """Everything is on the first page """ #####################
 
-recipe1 = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()
-recipe2 = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()
-recipe3 = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()
+recipe1 = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()  # Get my first recipe from
+# the json file
+recipe2 = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()  # Get my second recipe from
+# the json file
+recipe3 = requests.get('https://taco-1150.herokuapp.com/random/?full_taco=true').json()  # Get my third recipe from
+# the json file
 
-recipes_book = docx.Document()
-recipes_book.add_paragraph('Random Taco Cookbook', 'Title')
+recipes_book = docx.Document()  # Create a new word document
+recipes_book.add_paragraph('Random Taco Cookbook', 'Title')  # I add a new paragraph in this new document with
+# 'Random Taco Cookbook' as header text and with title style
 recipes_book.add_picture('Image_Tacos_last_modified.jpg', width=docx.shared.Inches(6),
-                         height=docx.shared.Inches(6))
-recipes_book.add_paragraph('Credits', 'Heading 1')
-recipes_book.add_paragraph('Taco image: Photo by Spencer Davis on Unsplash', style='List Bullet')
+                         height=docx.shared.Inches(6))  # I add the resized picture with 6 inches height and width
+recipes_book.add_paragraph('Credits', 'Heading 1')  # Add the text "Credits" with the style "Heading 1" (subtitle) in
+# a new paragraph
+
+recipes_book.add_paragraph('Taco image: Photo by Spencer Davis on Unsplash', style='List Bullet')  # New paragraph
+# with "Taco image..." with style bullet list
 recipes_book.add_paragraph('Tacos from: http://taco-randomizer.herokuapp.com/random/?full_taco=true', style='List '
                                                                                                             'Bullet')
-recipes_book.add_paragraph('Code by: Paseng Moua', style='List Bullet')
+# # New paragraph with text "Taco from..." with style bullet list
+recipes_book.add_paragraph('Code by: Paseng Moua', style='List Bullet')  # New paragraph with text "Code by: ..." with
+# style bullet list
 
-recipes_book.add_page_break()
-recipes_book.add_paragraph(f'{recipe1["base_layer"]["name"]} with {recipe1["seasoning"]["name"]}, {recipe1["condiment"]["name"]} and {recipe1["mixin"]["name"]} in {recipe1["shell"]["name"]}', 'Title')
-recipes_book.add_paragraph(f'{recipe1["base_layer"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe1["base_layer"]["recipe"]}')
-recipes_book.add_paragraph(f'Seasoning: {recipe1["seasoning"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe1["seasoning"]["recipe"]}')
-recipes_book.add_paragraph(f'Condiment: {recipe1["condiment"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe1["condiment"]["recipe"]}')
-recipes_book.add_paragraph(f'Mixin: {recipe1["mixin"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe1["mixin"]["recipe"]}')
-recipes_book.add_paragraph(f'Wrap: {recipe1["shell"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe1["shell"]["recipe"]}')
+##################      """Everything is on the second,third, forth.... page """ #####################
 
-recipes_book.add_page_break()
-recipes_book.add_paragraph(f'{recipe2["base_layer"]["name"]} with {recipe2["seasoning"]["name"]}, {recipe2["condiment"]["name"]} and {recipe2["mixin"]["name"]} in {recipe2["shell"]["name"]}', 'Title')
-recipes_book.add_paragraph(f'{recipe2["base_layer"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe2["base_layer"]["recipe"]}')
-recipes_book.add_paragraph(f'Seasoning: {recipe2["seasoning"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe2["seasoning"]["recipe"]}')
-recipes_book.add_paragraph(f'Condiment: {recipe2["condiment"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe2["condiment"]["recipe"]}')
-recipes_book.add_paragraph(f'Mixin: {recipe2["mixin"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe2["mixin"]["recipe"]}')
-recipes_book.add_paragraph(f'Wrap: {recipe2["shell"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe2["shell"]["recipe"]}')
+list_of_recipes = [recipe1, recipe2, recipe3]  # I create a list and I put my recipes in.
 
-recipes_book.add_page_break()
-recipes_book.add_paragraph(f'{recipe3["base_layer"]["name"]} with {recipe3["seasoning"]["name"]}, {recipe3["condiment"]["name"]} and {recipe3["mixin"]["name"]} in {recipe3["shell"]["name"]}', 'Title')
-recipes_book.add_paragraph(f'{recipe3["base_layer"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe3["base_layer"]["recipe"]}')
-recipes_book.add_paragraph(f'Seasoning: {recipe3["seasoning"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe3["seasoning"]["recipe"]}')
-recipes_book.add_paragraph(f'Condiment: {recipe3["condiment"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe3["condiment"]["recipe"]}')
-recipes_book.add_paragraph(f'Mixin: {recipe3["mixin"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe3["mixin"]["recipe"]}')
-recipes_book.add_paragraph(f'Wrap: {recipe3["shell"]["name"]}', 'Heading 1')
-recipes_book.add_paragraph(f'{recipe3["shell"]["recipe"]}')
+for recipe in list_of_recipes:  # For each recipe in my list of recipes
+    recipes_book.add_page_break()  # I add a new page
+    recipes_book.add_paragraph(
+        f'{recipe["base_layer"]["name"]} with {recipe["seasoning"]["name"]}, {recipe["condiment"]["name"]} and {recipe["mixin"]["name"]} in {recipe["shell"]["name"]}',
+        'Title')  # I put the title of the recipe with all the elements asked : base_layer, seasoning, condiment,
+    # mixin and shell as a 'Title'
+    recipes_book.add_paragraph(f'{recipe["base_layer"]["name"]}', 'Heading 1')  # I add a new paragraph and put the
+    # data in the recipe["base_layer"]["name"] as a subtitle 'Heading 1'
+    recipes_book.add_paragraph(f'{recipe["base_layer"]["recipe"]}')  # I add a new paragraph and put the
+    # data in the recipe["base_layer"]["recipe"] in the word document'
+    recipes_book.add_paragraph(f'Seasoning: {recipe["seasoning"]["name"]}', 'Heading 1') # I add a new paragraph and
+    # put the data in the recipe["seasoning"]["name"] as a subtitle 'Heading 1'
+    recipes_book.add_paragraph(f'{recipe["seasoning"]["recipe"]}') # I add a new paragraph and put the
+    # data in the recipe["seasoning"]["recipe"] in the word document'
+    recipes_book.add_paragraph(f'Condiment: {recipe["condiment"]["name"]}', 'Heading 1')
+    recipes_book.add_paragraph(f'{recipe["condiment"]["recipe"]}')
+    recipes_book.add_paragraph(f'Mixin: {recipe["mixin"]["name"]}', 'Heading 1')
+    recipes_book.add_paragraph(f'{recipe["mixin"]["recipe"]}')
+    recipes_book.add_paragraph(f'Wrap: {recipe["shell"]["name"]}', 'Heading 1')
+    recipes_book.add_paragraph(f'{recipe["shell"]["recipe"]}')
+
 recipes_book.save('Random Recipes Book.docx')
-
-#print(f'{recipe1["seasoning"]["recipe"]}')
-"""
-CODE TEST
-for i in recipe1['mixin'][data]:
-    recipes_book.add_paragraph(f'{data}', 'Heading 1')
-recipes_book.save('Random Recipes Book.docx')
-# for i in recipe1:
-#   print(i, recipe1[i])"""
-
-
